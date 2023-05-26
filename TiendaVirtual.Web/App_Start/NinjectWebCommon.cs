3099b9310f28b598e99cf31a342dffdc360ec8ac
pablofrers93 <pablofrers93@gmail.com>
2023-05-26 01:11:36 -0300

    lista paises

---
+++ TiendaVirtual.Web/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,74 @@
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(TiendaVirtual.Web.App_Start.NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(TiendaVirtual.Web.App_Start.NinjectWebCommon), "Stop")]
+
+namespace TiendaVirtual.Web.App_Start
+{
+    using System;
+    using System.Web;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Neptuno2022EF.Datos.Repositorios;
+    using Neptuno2022EF.Servicios.Servicios;
+    using Ninject;
+    using Ninject.Web.Common;
+    using Ninject.Web.Common.WebHost;
+    using TiendaVirtual.Datos;
+    using TiendaVirtual.Datos.Interfaces;
+    using TiendaVirtual.Servicios.Interfaces;
+
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+            kernel.Bind<NeptunoDbContext>().ToSelf().InRequestScope();
+            kernel.Bind<IRepositorioPaises>().To<RepositorioPaises>().InRequestScope();
+            kernel.Bind<IServiciosPaises>().To<ServiciosPaises>().InRequestScope();
+            kernel.Bind<IUnitOfWork>().To<UnitOfWork>().InRequestScope();
+        }
+    }
+}
---
+++ TiendaVirtual.Web/Controllers/PaisesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVirtual.Entidades.Entidades;
+using TiendaVirtual.Servicios.Interfaces;
+
+namespace TiendaVirtual.Web.Controllers
+{
+    public class PaisesController : Controller
+    {
+        // GET: Paises
+        private readonly IServiciosPaises _servicio;
+        public PaisesController(IServiciosPaises servicios)
+        {
+            _servicio = servicios;
+        }
+        public ActionResult Index()
+        {
+            //var lista = _servicio.GetPaises();
+            var lista = new List<Pais>();
+            return View(lista);
+        }
+    }
+}
