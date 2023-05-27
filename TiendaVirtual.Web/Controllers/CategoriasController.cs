cf22db413c0197fa584268791d68906225f2834a
pablofrers93 <pablofrers93@gmail.com>
2023-05-26 23:24:05 -0300

    edit, delete y new categoria

--- TiendaVirtual.Web/Controllers/CategoriasController.cs
+++ TiendaVirtual.Web/Controllers/CategoriasController.cs
@@ -28,16 +28,129 @@ namespace TiendaVirtual.Web.Controllers
             var listaVm = new List<CategoriaListVm>();
             foreach (var item in lista)
             {
-                var categoriaVm = new CategoriaListVm()
-                {
-                    CategoriaId = item.CategoriaId,
-                    NombreCategoria = item.NombreCategoria,
-                    Descripcion = item.Descripcion,
-                    RowVersion = item.RowVersion
-                };
+                var categoriaVm = GetCategoriaListVm(item);
                 listaVm.Add(categoriaVm);
             }
             return listaVm;
+        }
+        private CategoriaListVm GetCategoriaListVm(Categoria item)
+        {
+            return new CategoriaListVm()
+            {
+                CategoriaId = item.CategoriaId,
+                NombreCategoria = item.NombreCategoria,
+                Descripcion = item.Descripcion,
+                RowVersion = item.RowVersion
+            };
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CategoriaEditVm categoriaVm)
+        {
+            if (ModelState.IsValid)
+            {
+                var categoria = GetCategoriaFromCategoriaEditVm(categoriaVm);
+                if (_servicio.Existe(categoria))
+                {
+                    ModelState.AddModelError(string.Empty, "Categoría existente");
+                    return View(categoriaVm);
+                }
+                else
+                {
+                    _servicio.Guardar(categoria);
+                    TempData["Msg"] = "Registro guardado satisfactoriamente";
+                    return RedirectToAction("Index");
+                }                  
+            } 
+            else
+            {
+                return View(categoriaVm);
+            }
+        }
+        private Categoria GetCategoriaFromCategoriaEditVm(CategoriaEditVm categoriaEditVm)
+        {
+            return new Categoria()
+            {
+                CategoriaId = categoriaEditVm.CategoriaId,
+                NombreCategoria = categoriaEditVm.NombreCategoria,
+                Descripcion = categoriaEditVm.Descripcion,
+                RowVersion = categoriaEditVm.RowVersion
+            };
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var categoria = _servicio.GetCategoriaPorId(id.Value);
+            if (categoria == null)
+            {
+                return HttpNotFound("Código de categoría inexistente");
+            }
+            var categoriaVm = GetCategoriaListVm(categoria);
+            return View(categoriaVm);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm (int id)
+        {
+            var categoria = _servicio.GetCategoriaPorId(id);
+            if (_servicio.EstaRelacionado(categoria))
+            {
+                var categoriaVm = GetCategoriaListVm(categoria);
+                ModelState.AddModelError(string.Empty, "Categoría relacionada...Baja denegada");
+                return View(categoriaVm);
+            }
+            _servicio.Borrar(id);
+            TempData["Msg"] = "Registro borrado satisfactoriamente";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+            var categoria = _servicio.GetCategoriaPorId(id.Value);
+            if (categoria == null)
+            {
+                return HttpNotFound("Código de categoría inexistente");
+            }
+            var categoriaVm = GetCategoriaEditVmFromCategoria(categoria);
+            return View(categoriaVm);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit (CategoriaEditVm categoriaVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(categoriaVm);   
+            }
+            var categoria = GetCategoriaFromCategoriaEditVm(categoriaVm);
+            if (_servicio.Existe(categoria))
+            {
+                ModelState.AddModelError(string.Empty, "Categoría existente...");
+                return View(categoriaVm);
+            }
+            _servicio.Guardar(categoria);
+            TempData["Msg"] = "Registro editado satisfactoriamente";
+            return RedirectToAction("Index");
+        }
+        private CategoriaEditVm GetCategoriaEditVmFromCategoria(Categoria categoria)
+        {
+            return new CategoriaEditVm()
+            {
+                CategoriaId = categoria.CategoriaId,
+                NombreCategoria = categoria.NombreCategoria,
+                Descripcion = categoria.Descripcion,
+                RowVersion = categoria.RowVersion
+            };
         }
     }
 }
---
+++ TiendaVirtual.Web/ViewModels/Categoria/CategoriaEditVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TiendaVirtual.Web.ViewModels.Categoria
+{
+    public class CategoriaEditVm
+    {
+        public int CategoriaId { get; set; }
+
+        [DisplayName("Categoría")]
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [StringLength(100, ErrorMessage = "El campo {0} debe contener entre {2} y {1} caracteres", MinimumLength = 3)]
+        public string NombreCategoria { get; set; }
+        [StringLength(255, ErrorMessage = "El campo {0} debe contener entre {2} y {1} caracteres", MinimumLength = 3)]
+
+        public string Descripcion { get; set; }
+        public byte[] RowVersion { get; set; }
+    }
+}
